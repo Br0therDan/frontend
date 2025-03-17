7d025243fbde11e9e2cd8e36f555f0df9103c556
Dan Kim <daniel@mysingle.io>
2025-03-17 12:11:20 +0900

    add apps,versions,media_assetsfunctions

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -6,7 +6,7 @@ import {
   Configuration,
   OAuth2Api,
 } from '@/client/iam'
-import { PostsApi, CategoriesApi } from '@/client/blog'
+import { PostsApi, CategoriesApi } from '@/client/posts'
 import { DocsApi, CategoriesApi as CatApi } from '@/client/docs'
 import {
   AssetsApi,
@@ -41,7 +41,7 @@ const createConfig = (basePath: string) => {
 
 // ✅ 설정 객체 생성 (오류 수정)
 const iamConfiguration = createConfig(process.env.NEXT_PUBLIC_IAM_API_URL!)
-const blogConfiguration = createConfig(process.env.NEXT_PUBLIC_BLOG_API_URL!)
+const postsConfiguration = createConfig(process.env.NEXT_PUBLIC_POSTS_API_URL!)
 const docsConfiguration = createConfig(process.env.NEXT_PUBLIC_DOCS_API_URL!)
 const kidsConfiguration = createConfig(process.env.NEXT_PUBLIC_KIDS_API_URL!)
 const quantConfiguration = createConfig(process.env.NEXT_PUBLIC_QUANT_API_URL!)
@@ -52,8 +52,8 @@ export const UsersService = new UsersApi(iamConfiguration)
 export const AdminService = new AdminApi(iamConfiguration)
 export const OAuthService = new OAuth2Api(iamConfiguration)
 
-export const PostService = new PostsApi(blogConfiguration)
-export const CategoryService = new CategoriesApi(blogConfiguration)
+export const PostService = new PostsApi(postsConfiguration)
+export const CategoryService = new CategoriesApi(postsConfiguration)
 
 export const DocsService = new DocsApi(docsConfiguration)
 export const CatService = new CatApi(docsConfiguration)
