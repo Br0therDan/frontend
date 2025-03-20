76baf95da6cb3751d62869f74ab20d9faf3dfc35
Dan Kim <daniel@mysingle.io>
2025-03-20 22:13:08 +0900

    docker files updated

--- src/app/[locale]/main/posts/add/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-import PostForm from '@/components/services/postss/post/PostForm'
-import React from 'react'
-
-export default function AddPostPage() {
-  return <PostForm mode='add' />
-}
